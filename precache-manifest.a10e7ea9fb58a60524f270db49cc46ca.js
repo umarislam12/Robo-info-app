b94abb31979ef81d0b06c08fe7c08e8432f9b6d1
umar <f14b150@ibitpu.edu.pk>
2019-03-02 03:09:46 +0500

    Updates

---
+++ precache-manifest.a10e7ea9fb58a60524f270db49cc46ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4ae6fafb877198c4c704",
+    "url": "/static/js/main.4ae6fafb.chunk.js"
+  },
+  {
+    "revision": "1bea5a29dae978c3c1fe",
+    "url": "/static/js/1.1bea5a29.chunk.js"
+  },
+  {
+    "revision": "4ae6fafb877198c4c704",
+    "url": "/static/css/main.f4d3e8c5.chunk.css"
+  },
+  {
+    "revision": "1bea5a29dae978c3c1fe",
+    "url": "/static/css/1.16d0258e.chunk.css"
+  },
+  {
+    "revision": "38401b6bd6144c16c7e36d0fe130d325",
+    "url": "/index.html"
+  }
+];
