ab78f544a58b8915b21981f044308d39ccd26b76
umar <f14b150@ibitpu.edu.pk>
2019-03-02 03:22:43 +0500

    Updates

---
+++ precache-manifest.6873a8208860d2748d7673a4aa4b12c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo-info-app/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "5c89e02cce276d5946b0",
+    "url": "/Robo-info-app/static/js/runtime~main.5c89e02c.js"
+  },
+  {
+    "revision": "21c6c05ad27886a457fe",
+    "url": "/Robo-info-app/static/js/main.21c6c05a.chunk.js"
+  },
+  {
+    "revision": "572d59e8ceccef4dfeab",
+    "url": "/Robo-info-app/static/js/2.572d59e8.chunk.js"
+  },
+  {
+    "revision": "21c6c05ad27886a457fe",
+    "url": "/Robo-info-app/static/css/main.d7cb85d0.chunk.css"
+  },
+  {
+    "revision": "572d59e8ceccef4dfeab",
+    "url": "/Robo-info-app/static/css/2.1b95bf59.chunk.css"
+  },
+  {
+    "revision": "b3d02aa490b23bbc8ed39ffea09cee37",
+    "url": "/Robo-info-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a10e7ea9fb58a60524f270db49cc46ca.js"
+  "/Robo-info-app/precache-manifest.6873a8208860d2748d7673a4aa4b12c6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Robo-info-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
